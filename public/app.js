86935298f8a7267f3dcb9b6b497bb508282b30c8
Juraj420 <gavlakjuraj27@gmail.com>
2026-01-29 21:19:44 +0100

    Update datetime string handling in app.js
    Fix datetime string formatting by removing 'Z' suffix.

--- public/app.js
+++ public/app.js
@@ -96,7 +96,7 @@ document.getElementById("loadDataBtn").onclick = async () => {
 
   // Ak server posiela MySQL formát (bez Z)
   if (datetimeString.includes(" ")) {
-    datetimeString = datetimeString.replace(" ", "T") + "Z";
+    datetimeString = datetimeString.replace(" ", "T"); // bez Z!
   }
 
   const date = new Date(datetimeString);
@@ -113,6 +113,7 @@ document.getElementById("loadDataBtn").onclick = async () => {
     second: "2-digit"
   });
 }
+
 
 
 
